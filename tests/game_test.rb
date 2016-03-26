ec512ca001fd2a084ac5df4e9d2a7e9f576e0d71
Maciej Woźniak <maciekwo@gmail.com>
2016-03-26 17:28:54 +0100

    More tests for game

--- tests/game_test.rb
+++ tests/game_test.rb
@@ -17,11 +17,42 @@ class TestGame < Test::Unit::TestCase
   end
 
   def test_shot_save
-    user = Action.new(1,2)
-    computer = Action.new(1,2)
+    player1 = Action.new(1,2)
+    player2 = Action.new(1,2)
     game = Game.new
-    game.shot(user,computer)
+    game.shot(player1,player2)
     assert_equal(game.user_goals,0)
     assert_equal(game.computer_goals,0)
+  end
+
+  def test_shot_goal
+    player1 = Action.new(1,2)
+    player2 = Action.new(2,2)
+    game = Game.new
+    game.shot(player1,player2)
+    assert_not_equal(game.user_goals,game.computer_goals)
+  end
+
+  def test_end_game
+    game = Game.new
+    5.times do 
+      player1 = Action.new(1,2)
+      player2 = Action.new(1,2)
+      game.shot(player1,player2)
+      player1 = Action.new(1,2)
+      player2 = Action.new(2,2)
+      game.shot(player1,player2)
+    end
+    assert_equal(game.current_action, ActionTypes::ENDGAME)
+  end
+
+  def test_not_end_game
+    game = Game.new
+    10.times do 
+      player1 = Action.new(1,2)
+      player2 = Action.new(1,2)
+      game.shot(player1,player2)
+    end
+    assert_not_equal(game.current_action, ActionTypes::ENDGAME)
   end
 end
