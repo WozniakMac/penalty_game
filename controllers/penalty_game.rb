cde3186bdf4a96702030326ec70cc99162cc50a5
Maciej Woźniak <maciekwo@gmail.com>
2016-04-17 13:07:53 +0200

    Refactor cotroller errors

--- controllers/penalty_game.rb
+++ controllers/penalty_game.rb
@@ -20,27 +20,35 @@ class PenaltyGame < Sinatra::Base
     param :x, Integer, required: true
     param :y, Integer, required: true
 
-    action = Action.new(params[:x].to_s.to_i, params[:y].to_s.to_i)
-    return { errors: action.errors.first }.to_json unless action.valid?
-    if GameList.instance.has_game?(params[:id])
-      game = GameList.instance.find(params[:id])
-    else
-      return { errors: 'This game not exist' }.to_json
+    user = Action.new(params[:x].to_s.to_i, params[:y].to_s.to_i)
+    return { errors: user.errors.first }.to_json unless user.valid?
+    unless GameList.instance.has_game?(params[:id])
+      return PenaltyGameErrors.GAME_NOT_EXISTS
     end
-    x_rand = Random.new.rand(Action::XMIN..Action::XMAX)
-    y_rand = Random.new.rand(Action::YMIN..Action::YMAX)
-    computer = Action.new(x_rand, y_rand)
-    game.shot(action, computer)
-    return game.status.to_json
+    game = GameList.instance.find(params[:id])
+    computer = computer_action
+    game.shot(user, computer)
+    game.status.to_json
   end
 
   get '/:id/status.json' do
     content_type :json
     param :id, String, required: true
-    if GameList.instance.has_game?(params[:id])
-      return GameList.instance.find(params[:id]).status.to_json
-    else
-      return { errors: 'This game not exist' }.to_json
+    unless GameList.instance.has_game?(params[:id])
+      return PenaltyGameErrors.GAME_NOT_EXISTS
     end
+    GameList.instance.find(params[:id]).status.to_json
   end
+
+  private
+
+  def computer_action
+    x_rand = Random.new.rand(Action::XMIN..Action::XMAX)
+    y_rand = Random.new.rand(Action::YMIN..Action::YMAX)
+    Action.new(x_rand, y_rand)
+  end
+end
+
+class PenaltyGameErrors
+  GAME_NOT_EXISTS = { errors: 'This game not exist' }.to_json
 end
